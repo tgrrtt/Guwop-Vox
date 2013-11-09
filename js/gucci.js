be8637966c9fbab54af51582bbf89fd95a5fc4b2
Tyler Garrett <jtgarrett123@gmail.com>
2013-11-09 15:35:11 -0800

    Culled gucci faces to 4 for the time being, added more audio, text transition

--- js/gucci.js
+++ js/gucci.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
-    $("#chain img").on("click", function () {
+    $("#chain").on("click", function () {
         $("#chain-audio")[0].play();
     })
 
-    $("#privacy img").on("click", function () {
+    $("#privacy").on("click", function () {
         $("#privacy-audio")[0].play();
     })
 
-    $("#building img").on("click", function () {
+    $("#building").on("click", function () {
         $("#building-audio")[0].play();
+    })
+    $("#burr").on("click", function () {
+        $("#burr-audio")[0].play();
     })
 });
