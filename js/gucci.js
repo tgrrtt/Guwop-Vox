8df73f163b98032f2fe46c5c854b1e5fe0609630
Tyler Garrett <jtgarrett123@gmail.com>
2013-11-09 14:58:11 -0800

    Added jquery to git

--- js/gucci.js
+++ js/gucci.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-    $("#chain").on("click", function () {
+    $("#chain img").on("click", function () {
         $("#chain-audio")[0].play();
     })
-})
+
+    $("#privacy img").on("click", function () {
+        $("#privacy-audio")[0].play();
+    })
+
+    $("#building img").on("click", function () {
+        $("#building-audio")[0].play();
+    })
+});
