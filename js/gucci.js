b58d8d98869f8cf7bb9efdd7e9b6e4ccf0b3b5bb
Tyler Garrett <jtgarrett123@gmail.com>
2013-11-09 15:52:21 -0800

    fixed up some css, added burr to vcs

--- js/gucci.js
+++ js/gucci.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     $("#chain").on("click", function () {
         $("#chain-audio")[0].play();
-    })
+    });
 
     $("#privacy").on("click", function () {
         $("#privacy-audio")[0].play();
-    })
+    });
 
     $("#building").on("click", function () {
         $("#building-audio")[0].play();
-    })
+    });
     $("#burr").on("click", function () {
         $("#burr-audio")[0].play();
-    })
+    });
 });
