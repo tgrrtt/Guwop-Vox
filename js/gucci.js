fb6b5b68766f270ffdc59a9e44e65e402061f888
Tyler Garrett <jtgarrett123@gmail.com>
2013-11-09 23:38:35 -0800

    refactored audio js

--- js/gucci.js
+++ js/gucci.js
@@ -1,33 +1,14 @@
 $(document).ready(function () {
-    $("#chain").on("click", function () {
-        $("#chain-audio")[0].play();
+
+    $play = function (evt) {
+        var $thisAudio = $(evt.target)
+            .closest("div .face-container")
+            .find("audio");
+        $thisAudio[0].play();
+    };
+
+    $(".face-container").on("click", function (e) {
+        $play(e);
     });
 
-    $("#privacy").on("click", function () {
-        $("#privacy-audio")[0].play();
-    });
-
-    $("#building").on("click", function () {
-        $("#building-audio")[0].play();
-    });
-
-    $("#burr").on("click", function () {
-        $("#burr-audio")[0].play();
-    });
-
-    $("#wow").on("click", function () {
-        $("#wow-audio")[0].play();
-    });
-
-    $("#time").on("click", function () {
-        $("#time-audio")[0].play();
-    });
-
-    $("#airplane").on("click", function () {
-        $("#airplane-audio")[0].play();
-    });
-
-    $("#uhh").on("click", function () {
-        $("#uhh-audio")[0].play();
-    });
 });
