00ff23a31528f323ae1696c96430f9a7898230a7
Tyler Garrett <jtgarrett123@gmail.com>
2013-11-09 21:19:28 -0800

    Added 4 more heads back in, with audio.

--- js/gucci.js
+++ js/gucci.js
@@ -10,7 +10,24 @@ $(document).ready(function () {
     $("#building").on("click", function () {
         $("#building-audio")[0].play();
     });
+
     $("#burr").on("click", function () {
         $("#burr-audio")[0].play();
+    });
+
+    $("#wow").on("click", function () {
+        $("#wow-audio")[0].play();
+    });
+
+    $("#time").on("click", function () {
+        $("#time-audio")[0].play();
+    });
+
+    $("#airplane").on("click", function () {
+        $("#airplane-audio")[0].play();
+    });
+
+    $("#uhh").on("click", function () {
+        $("#uhh-audio")[0].play();
     });
 });
